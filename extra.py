a6a9ee8c935609dd5628ad7e0c9859447d3d93fc
ruthndiko43 <ruthndiko43@gmail.com>
2026-04-14 16:36:14 +0300

    extra

---
+++ extra.py
@@ -0,0 +1,22 @@
+number=1000
+print(type(number))
+number=str(number)
+print(type(number))
+number=float(number)
+print(type(number))
+print(number)
+
+number1=95.12345
+number1=round(number1,4)
+print(number1)
+
+
+name=input("My name is, ")
+print(name)
+
+# find sum of two numbers
+num1=int(input("Enter the first number"))
+num2=int(input("Enter the second number"))
+total=num1 + num2
+print(total)
+
