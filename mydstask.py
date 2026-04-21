2b43c1ba9e6e4a67c14c4d71680a6e07bf2d9ae8
ruthndiko43 <ruthndiko43@gmail.com>
2026-04-21 15:53:46 +0300

    mydstask

--- mydstask.py
+++ mydstask.py
@@ -4,6 +4,7 @@ my_ds = [23,"Jane",(560), ["Lesson", "Maths", {"currency" : "KES"}], 987, (76,"J
 print(my_ds[3][2]["currency"])
 #2. Print 560
 print(my_ds[2])
+print(type(my_ds[2]))
 #3. Print Maths
 print(my_ds[3][1])
 #4. In the dictionary with the key currency, add another key “amount” with value 90
@@ -14,10 +15,14 @@ print(my_ds)
 
 # reverse and assign back
 my_ds[4] = int(str(my_ds[4])[::-1])
-
+print(my_ds[4])
 print(my_ds)
 #6. Change the name “John” to “Jane” . 
-my_ds[1]="John"
+my_ds[5]=list(my_ds[5])
+#modify
+my_ds[5][1]='Jane'
+print(my_ds)
+my_ds[5]=tuple(my_ds[5])
 print(my_ds)
 #You can research or discuss to find the solutions above.
  #https://realpython.com/quizzes/pybasics-tuples-lists-dicts/ 
